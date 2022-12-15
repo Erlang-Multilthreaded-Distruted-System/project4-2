a19dc486d082804bcf7e735b8a52c7785404f29d
rl_zhu <rl_zhu@outlook.com>
2022-12-14 21:01:18 -0500

    get all, subscribe, register,getAllFollowingTweets

--- client/src/client.erl
+++ client/src/client.erl
@@ -2,12 +2,143 @@
 %%% @author lynn
 %%% @copyright (C) 2022, <COMPANY>
 %%% @doc
-%%%
 %%% @end
-%%% Created : 14. Dec 2022 12:26 PM
 %%%-------------------------------------------------------------------
 -module(client).
--author("lynn").
 
-%% API
--export([]).
+-behaviour(gen_server).
+
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+%% for test
+-export([getAllFollowingTweets/1,subscribe/2,get_all_users/0]).
+
+-define(SERVER, ?MODULE).
+
+-record(client_state, {}).
+
+-export([reigter/2]).
+
+
+%%%===================================================================
+%%% Spawning and gen_server implementation
+%%%===================================================================
+reigter(Username, Password) ->
+  SomeHostInNet = "localhost", % to make it runnable on one machine
+  {ok, Sock} = gen_tcp:connect(SomeHostInNet, 3456,
+    [binary, {packet, 4}, {active, false}]),
+  Msg = getJson_reg(Username, Password),
+  ok = gen_tcp:send(Sock, Msg),
+  gen_tcp:close(Sock),
+  receive_from_server_string(SomeHostInNet).
+
+getJson_reg(Username, Password) ->
+  UsernameBin = list_to_binary(Username),
+  PasswordBin = list_to_binary(Password),
+  Json = [{"Action" , <<"register">>}, {"Username" , UsernameBin}, {"Password", PasswordBin}],
+  iolist_to_binary(mochijson2:encode(Json)).
+
+
+getAllFollowingTweets(Username) ->
+  SomeHostInNet = "localhost", % to make it runnable on one machine
+  {ok, Sock} = gen_tcp:connect(SomeHostInNet, 3456,
+    [binary, {packet, 4}, {active, false}]),
+  Msg = getJson_getAllFollowingTweets(Username),
+  ok = gen_tcp:send(Sock, Msg),
+  gen_tcp:close(Sock),
+  receive_from_server(SomeHostInNet).
+
+getJson_getAllFollowingTweets(Username) ->
+  UsernameBin = list_to_binary(Username),
+  Json = [{"Action" , <<"getAllFollowingTweets">>}, {"Username" , UsernameBin}],
+  iolist_to_binary(mochijson2:encode(Json)).
+
+subscribe(Username, Password) ->
+  SomeHostInNet = "localhost", % to make it runnable on one machine
+  {ok, Sock} = gen_tcp:connect(SomeHostInNet, 3456,
+    [binary, {packet, 4}, {active, false}]),
+  Msg = getJson_subscribe(Username, Password),
+  ok = gen_tcp:send(Sock, Msg),
+  gen_tcp:close(Sock),
+  receive_from_server(SomeHostInNet).
+
+getJson_subscribe(Username, New_subscribe) ->
+  UsernameBin = list_to_binary(Username),
+  New_subscribeBin = list_to_binary(New_subscribe),
+  Json = [{"Action" , <<"subscribe">>}, {"Username" , UsernameBin}, {"New_subscribe", New_subscribeBin}],
+  iolist_to_binary(mochijson2:encode(Json)).
+
+
+
+
+get_all_users() ->
+
+  SomeHostInNet = "localhost", % to make it runnable on one machine
+  {ok, Sock} = gen_tcp:connect(SomeHostInNet, 3456,
+  [binary, {packet, 4}, {active, false}]),
+  Msg = getJson_get_all_users(),
+  ok = gen_tcp:send(Sock, Msg),
+  gen_tcp:close(Sock),
+  receive_from_server(SomeHostInNet).
+
+getJson_get_all_users() ->
+  Json = [{"Action" , <<"getAllUsers">>}, {"Username" , <<"getAllUsers">>}],
+  iolist_to_binary(mochijson2:encode(Json)).
+
+
+
+
+receive_from_server_string(SomeHostInNet) ->
+  {ok, SockB} = gen_tcp:connect(SomeHostInNet, 3456, [binary, {packet, 4}, {active, false}]),
+  {ok,B} = gen_tcp:recv(SockB,0),
+  io:format("From server ~s ~n", [B]),
+  ok = gen_tcp:close(SockB).
+
+receive_from_server(SomeHostInNet) ->
+  {ok, SockB} = gen_tcp:connect(SomeHostInNet, 3456, [binary, {packet, 4}, {active, false}]),
+  {ok,B} = gen_tcp:recv(SockB,0),
+  Dec = decode_api_list(B),
+  io:format("From server ~p ~n", [Dec]),
+  ok = gen_tcp:close(SockB).
+
+
+
+
+
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+  {ok, #client_state{}}.
+
+handle_call(_Request, _From, State = #client_state{}) ->
+  {reply, ok, State}.
+
+handle_cast(_Request, State = #client_state{}) ->
+  {noreply, State}.
+
+handle_info(_Info, State = #client_state{}) ->
+  {noreply, State}.
+
+terminate(_Reason, _State = #client_state{}) ->
+  ok.
+
+code_change(_OldVsn, State = #client_state{}, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
+encode_api(Json)->
+  iolist_to_binary(mochijson2:encode(Json)).
+
+
+decode_api_map(Bin) ->
+  {struct, Map} = mochijson2:decode(Bin),
+  Map.
+
+decode_api_list(Bin) ->
+  mochijson2:decode(Bin).
--- client/src/test.erl
+++ client/src/test.erl
@@ -4,10 +4,48 @@
 %%% @doc
 %%%
 %%% @end
-%%% Created : 14. Dec 2022 12:07 PM
+%%% Created : 14. Dec 2022 12:39 PM
 %%%-------------------------------------------------------------------
 -module(test).
 -author("lynn").
 
 %% API
--export([]).
+-export([client/0, test_str/0,add_string/0,test_case/0]).
+
+
+client() ->
+  {ok,Sock} = gen_tcp:connect("localhost",3434,[{active,false},
+    {packet,2}]),
+  gen_tcp:send(Sock, "client"),
+  A = gen_tcp:recv(Sock,0),
+  gen_tcp:close(Sock),
+  A.
+
+test_str() ->
+  Json = [{"Action" , <<"register">>}, {"Username" , <<"xiaozhu">>}, {"Password", <<"123456">>}],
+  Enco = iolist_to_binary(mochijson2:encode(Json)),
+  {struct, Map} = mochijson2:decode(Enco),
+  io:format(" the content fron client  ~p ~n", [Map]),
+  binary_to_list(proplists:get_value(<<"Action">>, Map)).
+
+add_string()->
+  A = "sd",
+  "dsdf" ++ A.
+
+test_case() ->
+  A = 1 > 2,
+  case A of
+    false ->
+      B =13,
+      io:format("false");
+    true ->
+      B =14,
+      io:format("true");
+    other ->
+      B =16,
+      io:format("other")
+  end,
+io:format("~w", [B]).
+
+
+map_get_toStr() ->
